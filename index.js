8e2eb23ff49f15352a1c3a51fecfdc0feb74c7bc
10Sadique <jafarsjahan@gmail.com>
2022-11-06 19:42:24 +0600

    events get method added

--- index.js
+++ index.js
@@ -11,13 +11,29 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB
-const uri =
-    'mongodb+srv://<username>:<password>@cluster0.onfc57d.mongodb.net/?retryWrites=true&w=majority';
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.onfc57d.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     serverApi: ServerApiVersion.v1,
 });
+
+async function run() {
+    const eventsCollection = client
+        .db('volunteerNetwork')
+        .collection('volunteerEvents');
+
+    // events
+    app.get('/events', async (req, res) => {
+        const query = {};
+        const cursor = eventsCollection.find(query);
+        const events = await cursor.toArray();
+
+        res.send(events);
+    });
+}
+
+run().catch((err) => console.dir);
 
 app.get('/', (req, res) => {
     res.send({
